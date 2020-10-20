9642dfc6f5e759f9f0b02d348f70d7efb0518573
Anastasia Chervona <av.chervona@gmail.com>
2020-10-20 12:38:41 +0300

    Updates

---
+++ precache-manifest.aabe818ebb5e9177a85e74e158327773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd25b812f212dd01806752e9c3ca57a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e49f788314d2c1362242",
+    "url": "./static/css/main.3aa9bd60.chunk.css"
+  },
+  {
+    "revision": "9cb276670027b79266c7",
+    "url": "./static/js/2.bafbf7af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bafbf7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e49f788314d2c1362242",
+    "url": "./static/js/main.ade4f78c.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
